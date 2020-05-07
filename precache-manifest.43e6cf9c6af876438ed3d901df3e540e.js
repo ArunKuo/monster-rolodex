23cb92d8c70be4e50b5195a750254f2062409d73
ArunKuo <steins1119@gmail.com>
2020-05-07 13:07:15 +0800

    Updates

---
+++ precache-manifest.43e6cf9c6af876438ed3d901df3e540e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e79db8046ce3fe60f4153f23433b51",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4bd9373dc1bbc4f49c0b",
+    "url": "/monster-rolodex/static/css/main.8915039f.chunk.css"
+  },
+  {
+    "revision": "0f3ddf253765cc2fb870",
+    "url": "/monster-rolodex/static/js/2.259823bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.259823bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd9373dc1bbc4f49c0b",
+    "url": "/monster-rolodex/static/js/main.37d50278.chunk.js"
+  },
+  {
+    "revision": "ade6441595e9e6da927e",
+    "url": "/monster-rolodex/static/js/runtime-main.040c738c.js"
+  }
+]);
